54ba9873ad96ebed3bf6cf85c0b01a6e78a0c655
alaspuresujay <alaspuresujay@gmail.com>
2021-04-19 23:09:46 +0530

    first commit

---
+++ Colors.js
@@ -0,0 +1,6 @@
+export default {
+	black: '#2d3436',
+	blue: '#24a6d9',
+	lightBlue: '#a7cbd9',
+	white: '#fff',
+};
---
+++ tempData.js
@@ -0,0 +1,60 @@
+export default [
+	{
+		name: 'Plan a Trip',
+		color: '#24a6d9',
+		todos: [
+			{
+				title: 'Book Flight',
+				completed: true,
+			},
+			{
+				title: 'Passport Check',
+				completed: false,
+			},
+			{
+				title: 'Book Hotel',
+				completed: true,
+			},
+			{
+				title: 'Pack Lugage',
+				completed: false,
+			},
+		],
+	},
+	{
+		name: 'Cancel a trip',
+		color: '#8022d9',
+		todos: [
+			{
+				title: 'cancel Flight',
+				completed: false,
+			},
+			{
+				title: 'Passport pack',
+				completed: true,
+			},
+			{
+				title: 'unBook Hotel',
+				completed: false,
+			},
+			{
+				title: 'unpack Lugage',
+				completed: false,
+			},
+		],
+	},
+	{
+		name: 'BirthDay',
+		color: '#a225a3',
+		todos: [
+			{
+				title: 'cancel Flight',
+				completed: false,
+			},
+			{
+				title: 'Passport pack',
+				completed: true,
+			},
+		],
+	},
+];
